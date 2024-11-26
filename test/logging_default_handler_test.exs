8b82e7db1df08c56ca421d441922b8a3ed0730c2
Victor <victor.kryukov@gmail.com>
2024-11-26 11:14:40 -0800

    More consistent naming, link to the github repo

--- test/logging_default_handler_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule LoggingDefaultHandlerTest do
-  use ExUnit.Case
-  doctest LoggingDefaultHandler
-
-  test "greets the world" do
-    assert LoggingDefaultHandler.hello() == :world
-  end
-end
